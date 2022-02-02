03c40c9dd4dd1a321e63e4352707e304383b856b
Fikunayom wyse <f0348@live.mdx.ac.uk>
2022-02-02 15:47:05 +0100

    third commit

---
+++ lessons.js
@@ -0,0 +1,102 @@
+let lessons = [
+    {
+        "id" : 1,
+        "subject": "Mathematics",
+        "location": "London",
+        "price": 250,
+        "spaces": 5,
+        "numberPurchased": 5,
+        "availableInventory" : 5,
+        "image": "images//Maths.jpg"
+    },
+    {
+        "id" : 2,
+        "subject": "Agriculture",
+        "location": "California",
+        "price": 150,
+        "spaces": 5,
+        "numberPurchased": 5,
+        "availableInventory" : 5,
+        "image": "images//product-Agric.jpg"
+  },
+  {
+        "id" : 3,
+        "subject": "Geography",
+        "location": "Mumbai",
+        "price": 150,
+        "spaces": 5,
+        "numberPurchased": 5,
+        "availableInventory" : 5,
+        "image": "images//product-geo.jpg"
+    },
+    {
+        "id" : 4,
+        "subject": "English",
+        "location": "Lagos",
+        "price": 250,
+        "spaces": 5,
+        "numberPurchased": 5,
+        "availableInventory" : 5,
+        "image": "images//product-english.jpg"
+    },
+    {
+        "id" : 5,
+        "subject": "Python",
+        "location": "Indonisia",
+        "price": 190,
+        "spaces": 5,
+        "numberPurchased": 5,
+        "availableInventory" : 5,
+        "image": "images//Python.png"
+    },
+    {
+        "id" : 6,
+        "subject": "Music",
+        "location": "Nevada",
+        "price": 100,
+        "spaces": 5,
+        "numberPurchased": 5,
+        "availableInventory" : 5,
+        "image": "images//product-music.jpg"
+    },
+    {
+        "id" : 7,
+        "subject": "Christian Religious Studies",
+        "location": "London",
+        "price": 110,
+        "spaces": 5,
+        "numberPurchased": 5,
+        "availableInventory" : 5,
+        "image": "images//CRK.jpg"
+    },
+    {
+        "id" : 8,
+        "subject": "Art",
+        "location": "Florida",
+        "price": 89,
+        "spaces": 5,
+        "numberPurchased": 5,
+        "availableInventory" : 5,
+        "image": "images//Art.jpg"
+    },
+    {
+        "id" : 9,
+        "subject": "BasketBall",
+        "location": "Cairo",
+        "price": 90,
+        "spaces": 5,
+        "numberPurchased": 5,
+        "availableInventory" : 5,
+        "image": "images//product-bball.jpg"
+    },
+    {
+        "id" : 10,
+        "subject": "Swimming",
+        "location": "Dubai",
+        "price": 90,
+        "spaces": 5,
+        "numberPurchased": 5,
+        "availableInventory" : 5,
+        "image": "images//product-swim.jpg"
+    }
+]
